372b89270d03e2aab461da21eaf53199a69517fe
makinci <merveeakinci97@gmail.com>
2020-05-08 00:59:47 +0300

    Web Interface created.

--- src/webui/src/app/app.module.ts
+++ src/webui/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import {AppLayoutComponent, FooterComponent, HeaderComponent, SidebarComponent} from "./_layout";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+export const createTranslateLoader = (http: HttpClient) => {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/webui/src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {AppLayoutComponent} from "./_layout";
+
+const routes: Routes = [
+  {
+    path: '',  component: AppLayoutComponent,
+    children: [
+      {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+      {path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardModule'},
+      {path: 'issue', loadChildren: './pages/issue/issue.module#IssueModule'},
+      {path: 'project', loadChildren: './pages/project/project.module#ProjectModule'}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
