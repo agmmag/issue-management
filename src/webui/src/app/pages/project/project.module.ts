14b6bce07d4aee00af1ce16bd7f502cbb53e4bea
makinci <merveeakinci97@gmail.com>
2020-05-10 00:39:05 +0300

    Localization eklendi.

--- src/webui/src/app/pages/project/project.module.ts
+++ src/webui/src/app/pages/project/project.module.ts
@@ -4,12 +4,14 @@ import {ProjectComponent} from './project.component';
 import {ProjectRoutingModule} from "./project.routing.module";
 import {ProjectService} from "../../services/shared/project.service";
 import {NgxDatatableModule} from "@swimlane/ngx-datatable";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     NgxDatatableModule,
-    ProjectRoutingModule
+    ProjectRoutingModule,
+    SharedModule
   ],
   providers: [ProjectService],
   declarations: [ProjectComponent]
