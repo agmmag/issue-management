6c7b0be8e1a23fc9edf0f03c46e8132bb96626ea
makinci <merveeakinci97@gmail.com>
2020-05-09 23:55:24 +0300

    Proje Listesi datatable'daki page number hatası düzeltildi.

--- src/webui/src/app/pages/project/project.component.ts
+++ src/webui/src/app/pages/project/project.component.ts
@@ -26,10 +26,10 @@ export class ProjectComponent implements OnInit {
   }
 
   setPage(pageInfo) {
-    this.page.number = pageInfo.offset;
+    this.page.page = pageInfo.offset;
     this.projectService.getAll(this.page).subscribe(pagedData => {
       this.page.size = pagedData.size;
-      this.page.number = pagedData.number;
+      this.page.page = pagedData.page;
       this.page.totalElements = pagedData.totalElements;
       this.rows = pagedData.content;
     });
