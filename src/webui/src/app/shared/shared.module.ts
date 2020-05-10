b2cbfa1339597d336a15733c03417fb4d6b67ae9
agsener <ahmetgokhansener@gmail.com>
2020-05-10 21:51:27 +0300

    Shared module ve package.json eklendi.

--- src/webui/src/app/shared/shared.module.ts
+++ src/webui/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {TranslateModule} from "@ngx-translate/core";
+import {ModalModule} from "ngx-bootstrap";
 
 @NgModule({
   exports:
     [
-      TranslateModule
+      TranslateModule,
+      ModalModule
     ]
 })
 export class SharedModule {
