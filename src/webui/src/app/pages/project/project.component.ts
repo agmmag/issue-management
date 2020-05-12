9a7830bbec33cb41f575122573cb7cade731e149
agsener <ahmetgokhansener@gmail.com>
2020-05-12 21:39:54 +0300

    Confirmation dialog (project silme) eklendi.

--- src/webui/src/app/pages/project/project.component.ts
+++ src/webui/src/app/pages/project/project.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from "@angular/core";
 import {ProjectService} from "../../services/shared/project.service";
 import {Page} from "../../common/page";
-import {Project} from "../../common/project.model";
+import {BsModalRef, BsModalService} from "ngx-bootstrap";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ConfirmationComponent} from "../../shared/confirmation/confirmation.component";
 
 @Component({
   selector: 'app-project',
@@ -9,20 +11,59 @@ import {Project} from "../../common/project.model";
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
+  modalRef: BsModalRef;
+  projectForm: FormGroup;
+
+  @ViewChild('tplProjectDeleteCell') tplProjectDeleteCell: TemplateRef<any>;
 
   page = new Page();
-  cols = [
-    {prop: 'id', name: 'No'},
-    {prop: 'projectName', name: 'Project Name', sortable: false},
-    {prop: 'projectCode', name: 'Project Code', sortable: false}];
-  rows = new Array<Project>();
+  cols = [];
+  rows = [];
 
-  constructor(private projectService: ProjectService) {
+  constructor(private projectService: ProjectService, private modalService: BsModalService, private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.cols = [
+      {prop: 'id', name: 'No'},
+      {prop: 'projectName', name: 'Project Name', sortable: false},
+      {prop: 'projectCode', name: 'Project Code', sortable: false},
+      {prop: 'id', name: 'Actions', cellTemplate: this.tplProjectDeleteCell, flexGrow: 1, sortable: false}
+    ];
 
     this.setPage({offset: 0});
+
+    this.projectForm = this.formBuilder.group({
+      'projectCode': [null, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      'projectName': [null, [Validators.required, Validators.minLength(4)]]
+    });
+
+  }
+
+  get f() {
+    return this.projectForm.controls
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  saveProject() {
+    if (!this.projectForm.valid)
+      return;
+
+    this.projectService.createProject(this.projectForm.value).subscribe(
+      response => {
+        console.log(response);
+      }
+    )
+    this.setPage({offset: 0});
+    this.closeAndResetModal();
+  }
+
+  closeAndResetModal() {
+    this.projectForm.reset();
+    this.modalRef.hide();
   }
 
   setPage(pageInfo) {
@@ -35,4 +76,24 @@ export class ProjectComponent implements OnInit {
     });
   }
 
+
+  showProjectDeleteConfirmation(value): void {
+    const modal = this.modalService.show(ConfirmationComponent);
+    (<ConfirmationComponent>modal.content).showConfirmation(
+      'Delete Confirmation',
+      'Are you sure for delete Project'
+    );
+
+    (<ConfirmationComponent>modal.content).onClose.subscribe(result => {
+        if (result === true) {
+          this.projectService.delete(value).subscribe(response => {
+            if (response === true) {
+              this.setPage({offset: 0})
+            }
+          });
+        } else if (result === false) {
+        }
+      }
+    );
+  }
 }
--- src/webui/src/app/shared/shared.module.ts
+++ src/webui/src/app/shared/shared.module.ts
@@ -1,13 +1,29 @@
-import {NgModule} from '@angular/core';
+import {NgModule} from "@angular/core";
 import {TranslateModule} from "@ngx-translate/core";
-import {ModalModule} from "ngx-bootstrap";
+import {BsModalRef, ModalModule} from "ngx-bootstrap";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ConfirmationComponent} from "./confirmation/confirmation.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-  exports:
-    [
-      TranslateModule,
-      ModalModule
-    ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ModalModule.forRoot()
+  ],
+  providers: [BsModalRef],
+  declarations: [
+    ConfirmationComponent
+  ],
+  entryComponents: [
+    ConfirmationComponent
+  ],
+  exports: [
+    TranslateModule,
+    ModalModule,
+    ReactiveFormsModule,
+    ConfirmationComponent
+  ]
 })
 export class SharedModule {
 }
